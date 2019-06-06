2954eb623be550fa1181efbe644cd03467364e5f
Nancy Granda Duarte <grandaduarte@wisc.edu>
2019-06-06 12:00:29 -0500

    makepdf

---
+++ makepdf.sh
@@ -0,0 +1,11 @@
+\#!/bin/bash
+
+cd src
+pdflatex mesh_rnucs_progress
+pdflatex mesh_rnucs_progress
+rm -rf *idx *out *.lof *.log *.lot *.dvi *.toc *.bbl *.blg *.aux
+
+mv mesh_rnucs_progress.pdf ../
+cd ../
+
+evince mesh_rnucs_progress.pdf
