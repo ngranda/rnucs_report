548deb93d2b11ad333ac495e7b82a778be26aa3b
Nancy <grandaduarte@wisc.edu>
2019-07-16 11:40:06 -0500

    build the bibliography

--- makepdf.sh
+++ makepdf.sh
@@ -2,6 +2,8 @@
 
 cd src
 pdflatex mesh_rnucs_progress
+bibtex mesh_rnucs_progress
+pdflatex mesh_rnucs_progress
 pdflatex mesh_rnucs_progress
 rm -rf *idx *out *.lof *.log *.lot *.dvi *.toc *.bbl *.blg *.aux
 
